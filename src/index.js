a6fb488c58c2387a61000183aaa64e21efac415b
Chris Perko <christopher.j.perko@windstream.com>
2017-08-21 11:32:57 -0500

    Section 1 complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAM6pHRYJJMJdopOkjk6AXTpJlIpmIvtx8';
 
 
 const App = () => {
   return (
-    <div>Hi! This is the app!</div>
+    <div>
+      <SearchBar />
+    </div>
   );
 }
 
