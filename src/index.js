89608dacddeff37532ad52e7ab79ee9de13f3254
Chris Perko <christopher.j.perko@windstream.com>
2017-08-23 11:09:28 -0500

    Complete section 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -13,7 +14,7 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { 
+    this.state = {
       videos: [],
       selectedVideo: null
     };
@@ -26,14 +27,16 @@ class App extends Component {
       this.setState({ videos, selectedVideo: videos[0] });
     });
   }
-  
+
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+    
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
-        <VideoList 
-          videos={this.state.videos} 
+        <VideoList
+          videos={this.state.videos}
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })} />
       </div>
     );
